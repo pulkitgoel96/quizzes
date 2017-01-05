abbdd4fd137fade6718b0c32d43638013e626f5c
shahadatthequint <shahadat.hussain@thequint.com>
2017-01-05 14:05:14 +0530

    Commented CSS

--- quintlab/gift-abu-a-burkha/index.php
+++ quintlab/gift-abu-a-burkha/index.php
@@ -17,7 +17,7 @@
 <link href="assets/css/base.css" rel="stylesheet" media="all">
 <link href="assets/css/theme.css" rel="stylesheet" media="all">
 
-<link href="css/main.css" rel="stylesheet" media="all">
+<!-- <link href="css/main.css" rel="stylesheet" media="all"> -->
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
