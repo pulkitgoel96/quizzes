42111737dbaccdcb4db20ba7e7bad81c4a075a2c
jaichanney <jai_channey@yahoo.com>
2017-02-08 12:41:50 +0530

    api-integration

---
+++ kamaal/elections-api-integration/assets/js/data-frame.js
@@ -0,0 +1,405 @@
+//Frame-chart-1
+
+$(document).ready(function(){
+	
+	// Common data
+	$('.count-table .table_title_1').text('Party');
+	$('.count-table .table_title_2').text('Per %');
+	$('.count-table .table_title_3').text('Total');
+	$('.count-table .table_title_4').text('Amount');
+	
+	
+	//For Table 1
+	
+	$('#table_1 .tr_1_td_1').text('data');
+	$('#table_1 .tr_1_td_2').text('data1');
+	$('#table_1 .tr_1_td_3').text('tbl-1');
+	$('#table_1 .tr_1_td_4').text('tbl-1');
+	
+	$('#table_1 .tr_2_td_1').text('tbl-1');
+	$('#table_1 .tr_2_td_2').text('tbl-1');
+	$('#table_1 .tr_2_td_3').text('tbl-1');
+	$('#table_1 .tr_2_td_4').text('tbl-1');
+	
+	$('#table_1 .tr_3_td_1').text('tbl-1');
+	$('#table_1 .tr_3_td_2').text('tbl-1');
+	$('#table_1 .tr_3_td_3').text('tbl-1');
+	$('#table_1 .tr_3_td_4').text('tbl-1');
+	
+	$('#table_1 .tr_4_td_1').text('tbl-1');
+	$('#table_1 .tr_4_td_2').text('tbl-1');
+	$('#table_1 .tr_4_td_3').text('tbl-1');
+	$('#table_1 .tr_4_td_4').text('tbl-1');
+	
+	$('#table_1 .table_result').text("'Party 1' Result text here");
+	
+	//For Table 2
+	
+	$('#table_2 .tr_1_td_1').text('tbl-2');
+	$('#table_2 .tr_1_td_2').text('tbl-2');
+	$('#table_2 .tr_1_td_3').text('tbl-2');
+	$('#table_2 .tr_1_td_4').text('tbl-2');
+	
+	$('#table_2 .tr_2_td_1').text('tbl-2');
+	$('#table_2 .tr_2_td_2').text('tbl-2');
+	$('#table_2 .tr_2_td_3').text('tbl-2');
+	$('#table_2 .tr_2_td_4').text('tbl-2');
+	
+	$('#table_2 .tr_3_td_1').text('tbl-2');
+	$('#table_2 .tr_3_td_2').text('tbl-2');
+	$('#table_2 .tr_3_td_3').text('tbl-2');
+	$('#table_2 .tr_3_td_4').text('tbl-2');
+	
+	$('#table_2 .tr_4_td_1').text('tbl-2');
+	$('#table_2 .tr_4_td_2').text('tbl-2');
+	$('#table_2 .tr_4_td_3').text('tbl-2');
+	$('#table_2 .tr_4_td_4').text('tbl-2');
+	
+	$('#table_2 .table_result').text("'Party 2' Result text here");
+	
+	//For Table 3
+	
+	$('#table_3 .tr_1_td_1').text('tbl-3');
+	$('#table_3 .tr_1_td_2').text('tbl-3');
+	$('#table_3 .tr_1_td_3').text('tbl-3');
+	$('#table_3 .tr_1_td_4').text('tbl-3');
+	
+	$('#table_3 .tr_2_td_1').text('tbl-3');
+	$('#table_3 .tr_2_td_2').text('tbl-3');
+	$('#table_3 .tr_2_td_3').text('tbl-3');
+	$('#table_3 .tr_2_td_4').text('tbl-3');
+	
+	$('#table_3 .tr_3_td_1').text('tbl-3');
+	$('#table_3 .tr_3_td_2').text('tbl-3');
+	$('#table_3 .tr_3_td_3').text('tbl-3');
+	$('#table_3 .tr_3_td_4').text('tbl-3');
+	
+	$('#table_3 .tr_4_td_1').text('tbl-3');
+	$('#table_3 .tr_4_td_2').text('tbl-3');
+	$('#table_3 .tr_4_td_3').text('tbl-3');
+	$('#table_3 .tr_4_td_4').text('tbl-3');
+	
+	$('#table_3 .table_result').text("'Party 3' Result text here");
+	
+	//For Table 4
+	
+	$('#table_4 .tr_1_td_1').text('tbl-4');
+	$('#table_4 .tr_1_td_2').text('tbl-4');
+	$('#table_4 .tr_1_td_3').text('tbl-4');
+	$('#table_4 .tr_1_td_4').text('tbl-4');
+	
+	$('#table_4 .tr_2_td_1').text('tbl-4');
+	$('#table_4 .tr_2_td_2').text('tbl-4');
+	$('#table_4 .tr_2_td_3').text('tbl-4');
+	$('#table_4 .tr_2_td_4').text('tbl-4');
+	
+	$('#table_4 .tr_3_td_1').text('tbl-4');
+	$('#table_4 .tr_3_td_2').text('tbl-4');
+	$('#table_4 .tr_3_td_3').text('tbl-4');
+	$('#table_4 .tr_3_td_4').text('tbl-4');
+	
+	$('#table_4 .tr_4_td_1').text('tbl-4');
+	$('#table_4 .tr_4_td_2').text('tbl-4');
+	$('#table_4 .tr_4_td_3').text('tbl-4');
+	$('#table_4 .tr_4_td_4').text('tbl-4');
+	
+	$('#table_4 .table_result').text("'Party 4' Result text here");
+	
+	
+	//For Table 5
+	
+	$('#table_5 .tr_1_td_1').text('tbl-5');
+	$('#table_5 .tr_1_td_2').text('tbl-5');
+	$('#table_5 .tr_1_td_3').text('tbl-5');
+	$('#table_5 .tr_1_td_4').text('tbl-5');
+	
+	$('#table_5 .tr_2_td_1').text('tbl-5');
+	$('#table_5 .tr_2_td_2').text('tbl-5');
+	$('#table_5 .tr_2_td_3').text('tbl-5');
+	$('#table_5 .tr_2_td_4').text('tbl-5');
+	
+	$('#table_5 .tr_3_td_1').text('tbl-5');
+	$('#table_5 .tr_3_td_2').text('tbl-5');
+	$('#table_5 .tr_3_td_3').text('tbl-5');
+	$('#table_5 .tr_3_td_4').text('tbl-5');
+	
+	$('#table_5 .tr_4_td_1').text('tbl-5');
+	$('#table_5 .tr_4_td_2').text('tbl-5');
+	$('#table_5 .tr_4_td_3').text('tbl-5');
+	$('#table_5 .tr_4_td_4').text('tbl-5');
+	
+	$('#table_5 .table_result').text("'Party 5' Result text here");
+	
+});
+
+//Frame-chart-1
+
+Highcharts.chart('frame-chart-1', {
+	colors: ['#461211', '#CB2826', '#FAA31B', '#FFCC06', '#D9B89C', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2015',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Firefox',   20],
+            ['IE',       20],
+            ['Chrome', 20],
+            ['Safari',    20],
+            ['Opera',     20],
+            {
+                name: 'Proprietary or Undetectable',
+                y: 0.2,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+});
+
+//Frame-chart-2
+Highcharts.chart('frame-chart-2', {
+	colors: ['#461211', '#CB2826', '#FAA31B', '#FFCC06', '#D9B89C', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2015',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Firefox',   10.38],
+            ['IE',       56.33],
+            ['Chrome', 24.03],
+            ['Safari',    4.77],
+            ['Opera',     0.91],
+            {
+                name: 'Proprietary or Undetectable',
+                y: 0.2,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+});
+
+//Frame-chart-3
+Highcharts.chart('frame-chart-3', {
+	colors: ['#461211', '#CB2826', '#FAA31B', '#FFCC06', '#D9B89C', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2015',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Firefox',   10.38],
+            ['IE',       56.33],
+            ['Chrome', 24.03],
+            ['Safari',    4.77],
+            ['Opera',     0.91],
+            {
+                name: 'Proprietary or Undetectable',
+                y: 0.2,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+});
+
+//Frame-chart-4
+Highcharts.chart('frame-chart-4', {
+	colors: ['#461211', '#CB2826', '#FAA31B', '#FFCC06', '#D9B89C', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2015',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Firefox',   10.38],
+            ['IE',       56.33],
+            ['Chrome', 24.03],
+            ['Safari',    4.77],
+            ['Opera',     0.91],
+            {
+                name: 'Proprietary or Undetectable',
+                y: 0.2,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+});
+
+
+//Frame-chart-5
+Highcharts.chart('frame-chart-5', {
+	colors: ['#461211', '#CB2826', '#FAA31B', '#FFCC06', '#D9B89C', '#aaeeee', '#ff0066', '#eeaaee',
+      '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+    title: {
+        text: 'Browser<br>shares<br>2015',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 40
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            dataLabels: {
+                enabled: true,
+                distance: -50,
+                style: {
+                    fontWeight: 'bold',
+                    color: 'white'
+                }
+            },
+            startAngle: -90,
+            endAngle: 90,
+            center: ['50%', '75%']
+        }
+    },
+    series: [{
+        type: 'pie',
+        name: 'Browser share',
+        innerSize: '50%',
+        data: [
+            ['Firefox',   25],
+            ['IE',       20],
+            ['Chrome', 30],
+            ['Safari',    10],
+            ['Opera',     15],
+            {
+                name: 'Proprietary or Undetectable',
+                y: 0.2,
+                dataLabels: {
+                    enabled: false
+                }
+            }
+        ]
+    }]
+});
+
---
+++ kamaal/elections-api-integration/assets/js/functions-frame.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var tableContent = $('.table-content').html();
+	var tableContent_1 = $('.table-content-1').html();
+	$('.box-info').html(tableContent);
+	$('.election-data').html(tableContent_1);
+	loadStories();
+});
+
+function loadStories(){
+	console.log("called2");
+	$.getJSON("test1.json?callback=?",abc);
+}
+
+function abc(j){
+	console.log(j);
+	}
+
