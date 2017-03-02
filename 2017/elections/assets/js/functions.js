8605d76dd4e9a27f9c5b8bf34d6a286a888f9242
jaichanney <jai_channey@yahoo.com>
2017-03-02 16:41:52 +0530

    s

--- 2017/elections/assets/js/functions.js
+++ 2017/elections/assets/js/functions.js
@@ -68,7 +68,7 @@ $(document).ready(function() {
 		  console.log(titleElement);
       }
       if(imageKey){
-	return '<div class="story-list"><div class="story-item"><a href="http://thequint-web.staging.quintype.io/' + lastStory.slug + '" target="blank"><figure><img src="http://images.assettype.com/' + imageKey + '?auto=format&amp;rect=0,0,2348,1321&amp;q=35&amp;w=270&amp;fm=pjpg" /><figcaption>' + titleElement.text + '</figcaption></figure></a></div></div>'
+	return '<div class="story-list"><div class="story-item"><a href="http://thequint-web.staging.quintype.io/' + lastStory.slug + '" target="blank"><figure><img src="https://images.assettype.com/' + imageKey + '?auto=format&amp;rect=0,0,2348,1321&amp;q=35&amp;w=270&amp;fm=pjpg" /><figcaption>' + titleElement.text + '</figcaption></figure></a></div></div>'
       }
     });
     elements.forEach(function(element) {
