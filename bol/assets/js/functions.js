862a601a4b666b826bffd55ad3a866274d3ab85c
Achintya De <achintya.de@thequint.com>
2017-07-03 15:36:10 +0530

    update bol

---
+++ bol/assets/js/functions.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function() {
+	$('.slider-1').slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: false,
+      autoplaySpeed: 4000,
+      responsive: [
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+			variableWidth: true
+            
+          }
+        }
+      ]
+    });
+	$('.slider-2').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: false,
+      autoplaySpeed: 4000,
+      responsive: [
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+			variableWidth: true
+            
+          }
+        }
+      ]
+    });
+	
+	$('.photo-gallery').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      arrows: false,
+      autoplaySpeed: 4000,
+    });
+});
+
+
+//WhatsApp
+$(document).ready(function() {
+  $(document).on("click", '#btnWp', function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      var text = $(this).attr("data-text");
+      var url = $(this).attr("data-link");
+      var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
+      var whatsapp_url = "whatsapp://send?text=" + message;
+      window.location.href = whatsapp_url;
+    } else {
+      alert("Please use an Mobile Device to Share this Article");
+    }
+  });
+});
+
