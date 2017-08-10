2f1a9d2fbc1a707836a60359ab3a5a72800bd2e0
shahadatthequint <shahadat.hussain@thequint.com>
2017-08-10 19:49:55 +0530

    Donor Man

---
+++ quintlab/donor-man/assets/js/functions.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $(".check-btn").click(function(){
+        $(".step-01").hide();
+        $(".step-02").show();
+        $(".source-content").show();
+        $(this).parent().hide();
+    });
+
+    $(".check-btn").click(function(){
+        $(".step-first").hide();
+        $(".step-second").show();
+
+        $(".is-error").hide();
+        $(".overlay").hide();
+        $(".msg-wrong").hide();
+        $(".msg-right").hide();
+    });
+
+    // Step 01
+    $(".step-01 .points").click(function(){
+        $(".is-show").hide();
+        $(this).children(".is-show").show();
+        $(".points").removeClass("active");
+        $(this).addClass("active");
+
+        $(".msg-right").fadeIn();
+        $(".overlay").show();
+    });
+
+    $(".try-btn, .step-01 .close").click(function(){
+        location.reload();
+    });
+
+    // Step 02
+    $(".step-02 .points").click(function(){
+        $(".is-show").hide();
+        $(this).children(".is-show").show();
+        $(".points").removeClass("active");
+        $(this).addClass("active");
+        adjust_iframe_height();
+    });
+
+    $(document).on('click',".close", function(){
+        $(".is-show").hide();
+        $(".points").removeClass("active");
+        //e.preventDefault();
+    });
+
+    // Document Click
+    $(document).mouseup(function (e){
+        var container = $(".pop-show");
+        if (!container.is(e.target)
+            && container.has(e.target).length === 0)
+        {
+            container.hide();
+        }
+        $(".points").removeClass("active");
+    });
+});
