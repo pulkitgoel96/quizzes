fe753314caa86d67139a20d1587be2dab219d51b
lab@thequint.com <shahadat.hussain@thequint.com>
2017-08-29 16:41:13 +0530

    o'clock

--- quintlab/chanderi/assets/js/functions.js
+++ quintlab/chanderi/assets/js/functions.js
@@ -4,14 +4,6 @@ $(document).ready(function() {
     $("#jama_masjid").click(function() {
         $("#jama_masjid_text").show();
         $("#jama_masjid_close").show();
-        // if ($(window).width() < 1024) {    
-        //     $("#jama_masjid_text").fadeIn();
-        //     $("#jama_masjid_close").fadeIn();
-        // }
-        // else {
-        //     $("#jama_masjid_text").addClass("circleEffect");
-        //     $("#jama_masjid_close").addClass("circleEffect");
-        // }
 
         $('html, body').animate({
             scrollTop:$('#jama_masjid_text').position().top - 80
@@ -21,26 +13,12 @@ $(document).ready(function() {
     $("#jama_masjid_close").click(function() {
         $("#jama_masjid_text").hide();
         $(this).hide();
-        // if ($(window).width() < 1024) { 
-        //     $("#jama_masjid_text").fadeOut();
-        //     $(this).fadeOut();
-        // }
-        // else {
-        //     $("#jama_masjid_text").removeClass("circleEffect");
-        //     $("#jama_masjid_close").removeClass("circleEffect");
-        // }
     });
 
     // Chanderi Saris
     $("#Chanderi_sari").click(function() {
-        if ($(window).width() < 1024) {    
-            $("#chanderi_sari_text").fadeIn();
-            $("#chanderi_sari_close").fadeIn();
-        }
-        else {
-            $("#chanderi_sari_text").addClass("circleEffect");
-            $("#chanderi_sari_close").addClass("circleEffect");
-        }
+        $("#chanderi_sari_text").show();
+        $("#chanderi_sari_close").show();
 
         $('html, body').animate({
             scrollTop:$('#chanderi_sari_text').position().top - 80
@@ -48,27 +26,14 @@ $(document).ready(function() {
     });
     // Close
     $("#chanderi_sari_close").click(function() {
-
-        if ($(window).width() < 1024) { 
-            $("#chanderi_sari_text").fadeOut();
-            $(this).fadeOut();
-        }
-        else {
-            $("#chanderi_sari_text").removeClass("circleEffect");
-            $("#chanderi_sari_close").removeClass("circleEffect");
-        }
+        $("#chanderi_sari_text").hide();
+        $(this).hide();
     });
 
     //A Centre of Jainism
     $("#a_centre_of_jainism").click(function() {
-        if ($(window).width() < 1024) {    
-            $("#a_centre_of_text").fadeIn();
-            $("#a_centre_of_close").fadeIn();
-        }
-        else {
-            $("#a_centre_of_text").addClass("circleEffect");
-            $("#a_centre_of_close").addClass("circleEffect");
-        }
+        $("#a_centre_of_text").show();
+        $("#a_centre_of_close").show();
 
         $('html, body').animate({
             scrollTop:$('#a_centre_of_text').position().top - 80
@@ -76,26 +41,14 @@ $(document).ready(function() {
     });
     // Close
     $("#a_centre_of_close").click(function() {
-        if ($(window).width() < 1024) { 
-            $("#a_centre_of_text").fadeOut();
-            $(this).fadeOut();
-        }
-        else {
-            $("#a_centre_of_text").removeClass("circleEffect");
-            $("#a_centre_of_close").removeClass("circleEffect");
-        }
+        $("#a_centre_of_text").hide();
+        $(this).hide();
     });
 
     // Chanderi Fort
     $("#chanderi_fort").click(function() {
-        if ($(window).width() < 1024) {    
-            $("#chanderi_fort_text").fadeIn();
-            $("#chanderi_fort_close").fadeIn();
-        }
-        else {
-            $("#chanderi_fort_text").addClass("circleEffect");
-            $("#chanderi_fort_close").addClass("circleEffect");
-        }
+        $("#chanderi_fort_text").show();
+        $("#chanderi_fort_close").show();
 
         $('html, body').animate({
             scrollTop:$('#chanderi_fort_text').position().top - 80
@@ -103,26 +56,14 @@ $(document).ready(function() {
     });
     // Close
     $("#chanderi_fort_close").click(function() {
-        if ($(window).width() < 1024) { 
-            $("#chanderi_fort_text").fadeOut();
-            $(this).fadeOut();
-        }
-        else {
-            $("#chanderi_fort_text").removeClass("circleEffect");
-            $("#chanderi_fort_close").removeClass("circleEffect");
-        }        
+        $("#chanderi_fort_text").hide();
+        $(this).hide();       
     });
 
     // A City of
     $("#a_city_of").click(function() {
-        if ($(window).width() < 1024) {    
-            $("#a_city_of_text").fadeIn();
-            $("#a_city_of_close").fadeIn();
-        }
-        else {
-            $("#a_city_of_text").addClass("circleEffect");
-            $("#a_city_of_close").addClass("circleEffect");
-        }        
+        $("#a_city_of_text").show();
+        $("#a_city_of_close").show();        
 
         $('html, body').animate({
             scrollTop:$('#a_city_of_text').position().top - 80
@@ -130,14 +71,8 @@ $(document).ready(function() {
     });
     // Close
     $("#a_city_of_close").click(function() {
-        if ($(window).width() < 1024) { 
-            $("#a_city_of_text").fadeOut();
-            $(this).fadeOut();
-        }
-        else {
-            $("#a_city_of_text").removeClass("circleEffect");
-            $("#a_city_of_close").removeClass("circleEffect");
-        }
+        $("#a_city_of_text").hide();
+        $(this).hide();
     });
 
 
@@ -152,14 +87,6 @@ $(document).ready(function() {
         else {
             $("#jama_masjid_text").hide();
             $("#jama_masjid_close").hide();
-            // if ($(window).width() < 1024) {    
-            //     $("#jama_masjid_text").fadeOut();
-            //     $("#jama_masjid_close").fadeOut();
-            // }
-            // else {
-            //     $("#jama_masjid_text").removeClass("circleEffect");
-            //     $("#jama_masjid_close").removeClass("circleEffect");
-            // }
         }
 
         if(evt.target.id == "chanderi_sari_text" || evt.target.id == "Chanderi_sari"){}
@@ -167,14 +94,8 @@ $(document).ready(function() {
         else if($(evt.target).closest('#chanderi_sari_text').length || $(evt.target).closest('#Chanderi_sari').length){}
             //console.log("yes")             
         else {
-            if ($(window).width() < 1024) {    
-                $("#chanderi_sari_text").fadeOut();
-                $("#chanderi_sari_close").fadeOut();
-            }
-            else {
-                $("#chanderi_sari_text").removeClass("circleEffect");
-                $("#chanderi_sari_close").removeClass("circleEffect");
-            }
+            $("#chanderi_sari_text").hide();
+            $("#chanderi_sari_close").hide();
         }
 
         if(evt.target.id == "a_centre_of_text" || evt.target.id == "a_centre_of_jainism"){}
@@ -182,14 +103,8 @@ $(document).ready(function() {
         else if($(evt.target).closest('#a_centre_of_text').length || $(evt.target).closest('#a_centre_of_jainism').length){}
             //console.log("yes")  
         else {
-            if ($(window).width() < 1024) {    
-                $("#a_centre_of_text").fadeOut();
-                $("#a_centre_of_close").fadeOut();
-            }
-            else {
-                $("#a_centre_of_text").removeClass("circleEffect");
-                $("#a_centre_of_close").removeClass("circleEffect");
-            }
+            $("#a_centre_of_text").hide();
+            $("#a_centre_of_close").hide();
         }
 
         if(evt.target.id == "chanderi_fort_text" || evt.target.id == "chanderi_fort") {}
@@ -197,14 +112,8 @@ $(document).ready(function() {
         else if($(evt.target).closest('#chanderi_fort_text').length || $(evt.target).closest('#chanderi_fort').length){}
             //console.log("yes")    
         else {
-            if ($(window).width() < 1024) {    
-                $("#chanderi_fort_text").fadeOut();
-                $("#chanderi_fort_close").fadeOut();
-            }
-            else {
-                $("#chanderi_fort_text").removeClass("circleEffect");
-                $("#chanderi_fort_close").removeClass("circleEffect");
-            }
+            $("#chanderi_fort_text").hide();
+            $("#chanderi_fort_close").hide();
         }
 
         if(evt.target.id == "a_city_of_text" || evt.target.id == "a_city_of"){}
@@ -212,14 +121,8 @@ $(document).ready(function() {
         else if($(evt.target).closest('#a_city_of_text').length || $(evt.target).closest('#a_city_of').length){}
             //console.log("yes")  
         else {
-            if ($(window).width() < 1024) {    
-                $("#a_city_of_text").fadeOut();
-                $("#a_city_of_close").fadeOut();
-            }
-            else {
-                $("#a_city_of_text").removeClass("circleEffect");
-                $("#a_city_of_close").removeClass("circleEffect");
-            }
+            $("#a_city_of_text").hide();
+            $("#a_city_of_close").hide();
         }
 
       //Do processing of click event here for every element except with id menu_content
