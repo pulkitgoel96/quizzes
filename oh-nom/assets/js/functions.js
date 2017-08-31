d5aa9d73428796f65ebcac434ac73878f439ac24
Achintya De <achintya.de@thequint.com>
2017-08-31 15:15:02 +0530

    update

--- oh-nom/assets/js/functions.js
+++ oh-nom/assets/js/functions.js
@@ -28,12 +28,15 @@ $(document).ready(function(){
 				$(".detail-content p").html(recipe_data[recipe_index].Intro_1);
 				$(".method").html(recipe_data[recipe_index].Steps_1);
 				$(".frame-video iframe").attr('src', recipe_data[recipe_index].Video_1);
+				$(".rcp-name").html(recipe_data[recipe_index].Item_1);
+				
 			}
 		else if($(this).index()==2)//right button
 			{
 				$(".detail-content p").html(recipe_data[recipe_index].Intro_2);
 				$(".method").html(recipe_data[recipe_index].Steps_2);
 				$(".frame-video iframe").attr('src', recipe_data[recipe_index].Video_2);
+				$(".rcp-name").html(recipe_data[recipe_index].Item_2);
 			}
 		
 		
